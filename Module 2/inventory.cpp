866f98889152b791702c6c56c716da0e645d6ea8
thuumaii <lettermine769@gmail.com>
2026-06-25 16:15:31 -0700

    inventory.cpp

---
+++ Module 2/inventory.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello Inventory System!" << endl;
+
+    cout << "\nData Types" << endl;
+    cout << "-------------" << endl;
+
+    cout << "int = " << sizeof(int) << " bytes" << endl;
+    cout << "float = " << sizeof(float) << " bytes" << endl;
+    cout << "char = " << sizeof(char) << " bytes" << endl;
+
+    cout << "\n\tEnd of Program" << endl;
+
+    return 0;
+}
