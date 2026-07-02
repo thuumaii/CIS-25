99e96304912d61d76be576dd80aef9f1400f33e5
thuumaii <lettermine769@gmail.com>
2026-07-02 07:45:49 -0700

    Add menu display and main loop in functions.cpp

---
+++ module 5/functions.cpp
@@ -0,0 +1,16 @@
+void showMenu()
+{
+    cout << "1. Add\n2. View\n3. Exit\n";
+}
+
+int main()
+{
+    int option;
+    do
+    {
+        showMenu();
+        cin >> option;
+    } while (option != 3);
+
+    return 0;
+}
